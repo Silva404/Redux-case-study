b6c6987aeedca7a07013b8aaf0dab42810d270d4
Silva404 <brenosilva0252@gmail.com>
2020-11-23 14:22:51 -0300

    feat: add redux rootReducer to use in store

---
+++ src/store/modules/rootReducer.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import { StoreState } from "../createStore";
+import auth from './auth/reducer'
+
+export default combineReducers<StoreState>({
+    auth
+})
