1f9ef9add37759ba4675b26f0802016b61394f67
Silva404 <brenosilva0252@gmail.com>
2020-11-23 14:23:29 -0300

    feat: add redux createStore to receive middlewares

---
+++ src/store/createStore.ts
@@ -0,0 +1,15 @@
+import { applyMiddleware, createStore, Middleware, Reducer } from "redux";
+import { AuthAction, AuthState } from "./modules/auth/types";
+
+
+export interface StoreState {
+    auth: AuthState
+}
+
+export type StoreAction = AuthAction
+
+export default (reducers: Reducer<StoreState, StoreAction>, middlewares: Middleware[]) => {
+    const enhancer = applyMiddleware(...middlewares);
+
+    return createStore(reducers, enhancer);
+}
